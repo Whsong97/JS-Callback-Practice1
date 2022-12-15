dd5a762327a85559f5b0088b5c3234fc043172e4
Wing <winghangsong@gmail.com>
2022-12-15 18:04:02 -0500

    JS-Callback-Practice

--- index.js
+++ index.js
@@ -13,3 +13,42 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+const charcacter = newImage('assets/green-character/static.gif')
+let direction = null;
+let x = 100;
+let y = 250;
+function setInterval () {
+if(direction === 'west') {
+    x = x - 1
+}
+if(direction === 'north') {
+    y = y + 1
+}
+   
+if(direction === 'east') {
+    x = x + 1
+}
+if(direction === 'south') {
+    y = y - 1
+}
+character.style.left = x + 'px'
+charcacter.style.bottom = y + 'px'
+}                                    
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
